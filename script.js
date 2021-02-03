840a739f048ff8055974b332367d4654533e1208
Neha <67725607+nehasoni05@users.noreply.github.com>
2021-02-03 13:44:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -48,3 +48,69 @@ function fun()
     }
 }
 }
+
+function java_e(x)
+{
+    x.src="./images/java_e.jpg";
+}
+function java(x)
+{
+    x.src="./images/java.png";
+}
+function c_e(x)
+{
+    x.src="./images/c_e.jpg";
+}
+function c(x)
+{
+    x.src="./images/c.png";
+}
+function html_e(x)
+{
+    x.src="./images/html_e.jpg";
+}
+function html(x)
+{
+    x.src="./images/html5.jpg";
+}
+
+function css_e(x)
+{
+    x.src="./images/css_e.jpg";
+}
+function css(x)
+{
+    x.src="./images/css.webp";
+}
+function js(x)
+{
+    x.src="./images/JavaScript-Logo.png";
+}
+function oops(x)
+{
+    x.src="./images/oops.jpg";
+}
+function ds_e(x)
+{
+    x.src="./images/ds_e.jpg";
+}
+function ds(x)
+{
+    x.src="./images/data-structures.jpg";
+}
+function dbms(x)
+{
+    x.src="./images/dbms.jpg";
+}
+function mysql(x)
+{
+    x.src="./images/mysql.png";
+}
+function oracle(x)
+{
+    x.src="./images/oracle-database.png";
+}
+function github(x)
+{
+    x.src="./images/GitHub-Mark.png";
+}
